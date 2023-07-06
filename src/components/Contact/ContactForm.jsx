748ccf4200b0906c86fab7c77d0fe8ce1159638c
hbashar434 <h.bashar434@gmail.com>
2023-07-06 22:06:19 +0600

    fixed skill

--- src/components/Contact/ContactForm.jsx
+++ src/components/Contact/ContactForm.jsx
@@ -127,7 +127,7 @@ const ContactForm = () => {
         type="submit"
         className="w-full rounded-md px-3.5 py-2 m-1 overflow-hidden relative group cursor-pointer border-2 font-medium border-indigo-600 text-indigo-600"
       >
-        <span className="absolute w-64 h-0 transition-all duration-300 origin-center rotate-45 -translate-x-20 bg-indigo-600 top-1/2 group-hover:h-64 group-hover:-translate-y-32 ease"></span>
+        <span className="absolute w-[900px] h-0 transition-all duration-300 origin-center rotate-45 -translate-x-72 bg-indigo-600 top-1/2 group-hover:h-[500px] group-hover:-translate-y-32 ease"></span>
         <span className="relative text-indigo-600 transition duration-300 group-hover:text-white ease">
           Send Message
         </span>
